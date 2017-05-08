3bb5b5583d6f3facc608e7473713af5b86bf729d
Andrew Klotz <agc.klotz@gmail.com>
2017-05-07 22:57:40 -0400

    mix phoenix.gen.channel Room

--- websocket_server/web/channels/user_socket.ex
+++ websocket_server/web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule WebsocketServer.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", WebsocketServer.RoomChannel
+  channel "room:*", WebsocketServer.RoomChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
