b25366ab5641a9b26950d948d8d253acad2b062a
Andrew Klotz <agc.klotz@gmail.com>
2017-05-30 23:39:48 -0400

    glow effect on isActive

--- frontend/src/containers/accountInfo/dropTarget.js
+++ frontend/src/containers/accountInfo/dropTarget.js
@@ -2,17 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { DropTarget } from 'react-dnd';
 import ItemTypes from '../../constants/itemTypes';
+import classNames from 'classnames/bind';
+import styles from './drop.css'
 
-const style = {
-  marginRight: '1.5rem',
-  marginBottom: '1.5rem',
-  color: 'white',
-  padding: '1rem',
-  textAlign: 'center',
-  fontSize: '1rem',
-  lineHeight: 'normal',
-  float: 'left',
-};
+let cx = classNames.bind(styles);
 
 const boxTarget = {
   drop(props) {
@@ -35,21 +28,10 @@ class DropContainer extends Component {
     const { canDrop, isOver, connectDropTarget } = this.props;
     const isActive = canDrop && isOver;
 
-    let backgroundColor = 'white';
-    if (isActive) {
-      backgroundColor = 'darkgreen';
-    } else if (canDrop) {
-      backgroundColor = 'darkkhaki';
-    }
-
     return connectDropTarget(
-      <div style={{ ...style, backgroundColor }}>
-        {isActive ?
-          'Release to drop' :
-          'Drag a box here'
-        }
+      <span className={cx({base: true, active: isActive})}>
         {this.props.children}
-      </div>,
+      </span>,
     );
   }
 }
